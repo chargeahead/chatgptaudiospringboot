6bab8d25ae5031ce5174923b66855da37d604646
pankajjain <llcchargeahead@gmail.com>
2023-08-05 20:54:16 -0700

    Initial version

--- src/main/java/com/example/chatgptspringboot/controller/MyController.java
+++ src/main/java/com/example/chatgptspringboot/controller/MyController.java
@@ -1,43 +1,25 @@
 package com.example.chatgptspringboot.controller;
 
-import com.theokanning.openai.completion.chat.ChatCompletionChoice;
-import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.image.CreateImageRequest;
+import com.theokanning.openai.image.Image;
 import com.theokanning.openai.service.OpenAiService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 public class MyController {
-  @Value("${openai.model}")
-    private String model;
-  @Value("${openai.api.key}")
+    @Value("${openai.api.key}")
     private String apiKey;
 
-  @GetMapping("/chat")
-    public String chat(@RequestParam String prompt){
-      OpenAiService service = new OpenAiService(apiKey);
-      List<ChatMessage> messages = new ArrayList<ChatMessage>();
-      ChatMessage message = new ChatMessage("user",prompt);
-      messages.add(message);
-      ChatCompletionRequest completionRequest = ChatCompletionRequest.
-              builder()
-              .messages(messages)
-              .model(model)
-              .temperature(0.6)
-              .n(1)
-              .build();
-      List<ChatCompletionChoice> choices = service.createChatCompletion(completionRequest).getChoices();
-      String returnString = "";
-      for (ChatCompletionChoice choice:choices){
-          returnString += "response: "+choice.getMessage().getContent()+System.lineSeparator();
-      }
-      return returnString;
-
-  }
+    @GetMapping("/image")
+    public String image(@RequestParam String prompt){
+        OpenAiService service = new OpenAiService(apiKey);
+        CreateImageRequest req = new CreateImageRequest();
+        req.setPrompt(prompt);
+        req.setSize("1024x1024");
+        Image image = service.createImage(req).getData().get(0);
+        return image.getUrl();
+    }
 }
